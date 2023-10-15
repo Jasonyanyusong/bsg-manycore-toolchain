940fac85f44dd5f078f2f9530088a4c1dc1e4f19
Jasonyanyusong <jasonyys@uw.edu>
2023-10-15 11:39:57 -0700

    bare-metal: add ISA dependent scripts

---
+++ bare-metal/arch-dependent/riscv32.mk
@@ -0,0 +1,8 @@
+# ISA related makefile, for RISC-V user can change the desired ISA and ABI
+# If the deisgn has no FPR regfile, you can remove f or d in the mabi
+
+ISA_COMPILER			= riscv64-linux-gnu-
+ISA_COMPILER_FLAGS		:= -fno-pic -march=rv32g -mabi=ilp32f
+ISA_COMPILER_CFLAGS		+= $(ISA_COMPILER_FLAGS) -static
+ISA_COMPILER_ASFLAGS	+= $(ISA_COMPILER_FLAGS) -O0
+ISA_COMPILER_LDFLAGS	+= -melf32lriscv
